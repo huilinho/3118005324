63ae7e1ed0888580efa66977581aef31dc49541d
huilin <1647529134@qq.com>
2020-09-21 14:51:47 +0800

    first commit

---
+++ src/com/code/TextException.java
@@ -0,0 +1,9 @@
+package com.code;
+
+/**
+ * @author huilin
+ * @version 1.0
+ * @date 2020/9/21 14:27
+ */
+public class TextException {
+}
---
+++ src/com/code/main.java
@@ -0,0 +1,9 @@
+package com.code;
+
+/**
+ * @author huilin
+ * @version 1.0
+ * @date 2020/9/21 14:26
+ */
+public class main {
+}
