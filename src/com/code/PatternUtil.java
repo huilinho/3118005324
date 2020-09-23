4e670bfc23bc12de2a89c835ebdb5409eeea9488
huilin <1647529134@qq.com>
2020-09-23 15:38:43 +0800

    fix:修改文件处理工具类

--- src/com/code/PatternUtil.java
+++ src/com/code/PatternUtil.java
@@ -12,11 +12,12 @@ public class PatternUtil {
     /***
      * 去除空格等非中英文数字字符
      */
-    public static void PreProgress(char[] originText,char[] destText) {
+    public static char[] PreProgress(String text) {
         //去除文本中所有非中文英文数字的字符
         String patternText = "[\\u3002|\\uff1f|\\uff01|\\uff0c|\\u3001|\\uff1b|\\uff1a|\\u201c|\\u201d|\\u2018|\\u2019|\\uff08|\\uff09|\\u300a|\\u300b|\\u3008|\\u3009|\\u3010|\\u3011|\\u300e|\\u300f|\\u300c|\\u300d|\\ufe43|\\ufe44|\\u3014|\\u3015|\\u2026|\\u2014|\\uff5e|\\ufe4f|\\uffe5|\\uff0d \\uff3f|\\u002d]?";
         Pattern pattern = Pattern.compile(patternText);
-        originText = pattern.matcher(new String(originText)).replaceAll("").trim().toCharArray();
-        destText = pattern.matcher(new String(destText)).replaceAll("").trim().toCharArray();
+        text = pattern.matcher(text).replaceAll("").trim();
+        char[] result = text.toCharArray();
+        return result;
     }
 }
