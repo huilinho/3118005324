508302ee6f948fea8d2f38b045142b91d4ccbf96
huilin <1647529134@qq.com>
2020-09-24 13:19:04 +0800

    fix:修改主程序bug，生成main.jar

--- src/com/code/main.java
+++ src/com/code/main.java
@@ -21,11 +21,11 @@ public class main {
              //如果输入了第三个参数，就将结果写入第三个参数所指的文件
              if(args.length>2 && args[2]!=null){
                  FileProcessor.stringToFile(result,args[2]);
-                 System.out.format("结果已成功写入文件:%s 中",args[3]);
+                 System.out.format("结果已成功写入文件:%s 中",args[2]);
              }
              //如果没有输入第三个参数，将结果打印在控制台上
              else{
-                 System.out.format("%f",result);
+                 System.out.println(result);
              }
         }
     }
